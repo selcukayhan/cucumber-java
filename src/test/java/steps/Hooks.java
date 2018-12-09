8a75dbd9c918d4532f52c4e6715c9fd8fb7394d6
selcukayhan <selcukayhann@hotmail.com>
2018-12-10 01:33:33 +0300

    imports optimized

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -2,9 +2,6 @@ package steps;
 
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
