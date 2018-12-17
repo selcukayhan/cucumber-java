cdbcec13e5cbb4722647daff44e751b13090c409
selcukayhan <selcukayhann@hotmail.com>
2018-12-17 03:02:49 +0300

    mvn updated

--- src/test/java/configuration/RunCukesTest.java
+++ src/test/java/configuration/RunCukesTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = {"src/test/resources/features/features/"},
+        features = {"src/test/java/features/"},
         glue = {"steps"}
 )
 public class RunCukesTest {
