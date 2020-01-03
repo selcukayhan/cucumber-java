1cc1577826dbde492de130772dc0da9bca833ecf
selcuk ayhan <selcuk.ayhan@mediamonks.com>
2020-01-03 14:01:53 +0100

    idea added to gitIgnore webdriver converted to the remoteWebdriver. runEnv="docker" set. Remote driver is set for docker=selenium-grid

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A small example on how to use cucumber &amp; selenium in java with Page Object M
 
 ## Usage
 You can run the tests by either executing mvn test or running the specified feature file. 
+
+If you run the tests with env variable runEnv="docker" , tests will be sent to the selenium grid. 
+
 ## Before Test
 This tests for amazon.com,
 Please update credentials in Config file ( Credentials and DROPBOX_ACCESS_TOKEN )
--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -5,15 +5,32 @@ import cucumber.api.java.After;
 import cucumber.api.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.rmi.Remote;
 
 public class Hooks {
 
     public static WebDriver driver;
+    public static String runEnv = System.getProperty("runEnv").toLowerCase();
 
     @Before
-    public void setUp() {
-        driver = new ChromeDriver();
-        driver.get(Config.MAIN_URL);
+    public void setUp() throws Exception {
+        switch (runEnv) {
+            case "docker":
+                driver = getRemoteDriver();
+                driver.get(Config.MAIN_URL);
+                break;
+            default:
+                driver = getLocalDriver();
+                driver.get(Config.MAIN_URL);
+                break;
+        }
     }
 
     @After
@@ -21,4 +38,25 @@ public class Hooks {
         driver.quit();
     }
 
+    public WebDriver getRemoteDriver() throws Exception {
+        try {
+            return new RemoteWebDriver(new URL("http://127.0.0.1:4444"),remoteDriverCapabilities());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public WebDriver getLocalDriver() {
+        return new ChromeDriver();
+    }
+
+    public DesiredCapabilities remoteDriverCapabilities(){
+        new DesiredCapabilities();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        chromeOptions.addArguments("window-size=1280,1024");
+        capabilities.setCapability(ChromeOptions.CAPABILITY,chromeOptions);
+        return capabilities;
+    }
 }
